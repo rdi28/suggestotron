816dbae70c26dcad0a83feaf14db5d0b69e8afb3
Ron Di <ron@indiegogo.com>
2013-12-29 12:01:02 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Suggestotron::Application.config.secret_key_base = 'b489d43be240356c4b14b4edc460c2b2576214da8cd974d9acd2aa6648c9323a1882ad26533c6508c535c6fe22552708b3e0fa3e10fd3e3504b43b8a31970eb6'
